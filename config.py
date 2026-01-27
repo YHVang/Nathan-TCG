9591b0a70d8229b987eab8b124a903668b545e01
unknown <vang3827@umn.edu>
2026-01-27 17:23:29 -0600

    minor updats to connection db.

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import os
-from dotenv import load_dotenv
+import psycopg2
+from config import DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT
 
-# Load .env ONLY for local development
-load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "tcg_website_db")
-DB_USER = os.getenv("DB_USER", "tcg_website_user")
-DB_PASS = os.getenv("DB_PASS", "password")
-DB_PORT = int(os.getenv("DB_PORT", 5432))
+if DATABASE_URL:
+    # Render / production
+    conn = psycopg2.connect(DATABASE_URL)
+else:
+    # Local development
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        port=DB_PORT
+    )
